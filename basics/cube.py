896a9a8a34f468e9e9231a95792b49067ab9f96d
Abhinav-1303 <abhinavgireesh0495@gmail.com>
2025-07-13 21:42:57 +0530

    Initial commit

--- basics/cube.py
+++
@@ -1,6 +0,0 @@
-def cube(num):
-    return num*num*num
-
-user_input = float(input("Enter the number: "))
-result = cube(user_input)
-print(result)
--- basics/tuples.py
+++
@@ -1,5 +0,0 @@
-coordinates=(2,4)#coordinates cannot be modified
-print(coordinates)
-
-coordinates=[(3,4),(6,8),(1,3)]
-print(coordinates)
